588c5f80d6747424935ee810430a1affa03b8f10
Zhili He <1356738145@qq.com>
2025-12-13 09:35:15 +0800

    Add next.config.js to exclude problematic test files from build

---
+++ frontend/next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Exclude test files from certain packages that incorrectly include them
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    // Add plugin to ignore problematic modules
+    config.plugins.push(
+      new config.webpack.IgnorePlugin({
+        resourceRegExp: /^(tap|tape|why-is-node-running)$/,
+      })
+    );
+
+    // Ignore specific test files
+    config.externals = config.externals || [];
+    if (!isServer) {
+      config.externals.push({
+        'thread-stream/test/transpiled.test.js': '{}',
+        'thread-stream/test/indexes.js': '{}',
+        'thread-stream/test/helper.js': '{}',
+      });
+    }
+
+    return config;
+  },
+  // Exclude problematic packages from server components
+  experimental: {
+    serverComponentsExternalPackages: ['pino', 'thread-stream'],
+  },
+};
+
+module.exports = nextConfig;
